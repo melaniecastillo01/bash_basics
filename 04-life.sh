f0417492e6b39e88745a50dfaa9d5b0a4c9fd135
Melanie Castillo <mcastillo3@ggc.edu>
2026-08-17 20:46:48 -0400

    Add morning check to 04-life script

--- 04-life.sh
+++ 04-life.sh
@@ -14,3 +14,11 @@ fi
 
 # exercise: write a script that prints whether it is 
 # morning or not
+hour=$(date +%H)
+
+if [ "$hour" -lt 12 ]; then
+    echo "Good morning!"
+else
+    echo "It is not morning."
+fi
+
